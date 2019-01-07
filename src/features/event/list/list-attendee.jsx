8e7474b5d9c01de8d52b1d3c0e5d5ce9ebfb8620
Tim Vaneker <timvaneker@outlook.com>
2019-01-07 13:55:28 +0100

    Built static version

--- src/features/event/list/list-attendee.jsx
+++ src/features/event/list/list-attendee.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { List, Image } from 'semantic-ui-react';
 
-export default function ListAttendee() {
+export default function ListAttendee(props) {
+  const { attendee } = props;
   return (
     <List.Item>
-      <Image
-        as="a"
-        size="mini"
-        circular
-        src="https://randomuser.me/api/portraits/women/42.jpg"
-      />
+      <Image as="a" size="mini" circular src={attendee.photoURL} />
     </List.Item>
   );
 }
--- src/features/event/list/list.jsx
+++ src/features/event/list/list.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import EventListItem from './list-item';
 
-export default function List() {
+export default function List(props) {
+  const { events } = props;
   return (
     <div>
       <h1>Event List</h1>
-      <EventListItem />
-      <EventListItem />
-      <EventListItem />
+      {events.map(event => (
+        <EventListItem key={event.id} event={event} />
+      ))}
     </div>
   );
 }
